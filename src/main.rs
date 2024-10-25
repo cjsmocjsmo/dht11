993e4ae0e5dfd51268eec3b1054af18208785da6
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-10-25 08:33:52 -0700

    Update GPIO line number for DHT sensor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use dht_mmap_rust::{Dht, DhtType};
 
 fn main() {
     // The sensor is a DHT11 connected on pin 23
-    let mut dht = Dht::new(DhtType::Dht11, 23).unwrap();
+    let mut dht = Dht::new(DhtType::Dht11, 2).unwrap();
 
     // Important: DHT sensor reads fail sometimes. In an actual program, if a read fails you should retry multiple times until
     // the read succeeds.
